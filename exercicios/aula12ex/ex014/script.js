dc0e2c89e97737b7ab8300f43ab8553707fc5e30
celenny <celennycs@gmail.com>
2021-08-04 13:59:01 -0300

    class

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -3,16 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são <strong>${hora} horas.</strong>`
 
     if (hora >= 0 && hora < 12) {
         // manhã
         img.src = 'manha.png'
+        document.body.style.background = '#FFBC93'
     } else if (hora > 12 && hora < 18) {
         // tarde
         img.src = 'tarde.png'
+        document.body.style.background = '#E97D3E'
     } else {
         // noite
         img.src = 'noite.png'
+        document.body.style.background = '#2A88A2'
     }
 }
