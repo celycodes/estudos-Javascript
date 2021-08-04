55e928c8d06f11d3fadb95441ad1c3712e86b69b
celenny <celennycs@gmail.com>
2021-08-03 21:34:13 -0300

    class

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var veloc = 60.5
+console.log(`A velocidade do seu carro é ${veloc}km/h`)
+if (veloc > 60) { // condição simples
+    console.log('MULTADO !! Você ultrapassou a velocidade PERMITIDA')
+}
+console.log('Dirija semmpre usando cinto de segurança!!')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Canada'
+
+if (country == 'Brazil') {
+    console.log('You are a Brazilian!')
+} else {
+    console.log(`So you are from ${country}`)
+}
