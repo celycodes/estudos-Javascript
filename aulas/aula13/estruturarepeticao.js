ba1d7ea0ce84386f73de8a3c685fd6f05be85e4c
celenny <celennycs@gmail.com>
2021-08-13 21:56:53 -0300

    aula13

---
+++ aulas/aula13/estruturarepeticao.js
@@ -0,0 +1,16 @@
+// estrutura de repedição parte 1
+
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+while (c <= 12) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+for (var i = 13;i <= 18; i++) {
+    console.log(`passo ${i}`)
+}
